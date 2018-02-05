2fddfe9fc98b667d9a9b18ae744bf8b237cf5803
hotWing17 <Lajichi77@163.com>
2018-02-05 19:14:13 +0800

    完成爬虫练习(一)2.0功能

--- 爬虫/爬虫练习(一)/实例_2.py
+++ 爬虫/爬虫练习(一)/实例_2.py
@@ -17,10 +17,6 @@ def getHTMLText(url):
     except:
         print('getHTMLText()出错！')
 
-#从首页获取单独套图的URL，返回一个列表
-def getALLlist(T_url):
-    print('')
-
 #从套图html内容解析出每张图片下载地址列表(url:该套图url)
 def getPictureUrl_List(List, url):
     html = getHTMLText(url)
@@ -45,7 +41,7 @@ def downloadPicture(List, path):
         if not os.path.exists(path):
             os.mkdir(path)
         for i in range(len(List)):
-            name = root + str(i+1) + '.jpg'
+            name = path + str(i+1) + '.jpg'
             if not os.path.exists(name):
 
                 r = requests.get(List[i])
@@ -54,29 +50,50 @@ def downloadPicture(List, path):
                 print('\r第 ' + str(i+1) + ' 张图片成功下载！',end = '')
             else:
                 print('文件已经存在')
+        print('')
+        print('')
     except:
         traceback.print_exc()
         print('downloadPicture()出错！')
-    
-#下载该页面所有套图方法
-def downloadALL(T_List, root):
-    List = []
-    try:
-        for i in range(len(T_List))
-        getPictureUrl_List(List, T_List[i])
-        downloadPicture(List, path)
 
+#从首页获取单独套图的URL，返回一个列表
+def getALLlist(T_list, T_url):
+    html = getHTMLText(T_url)
+    try:
+        soup = BeautifulSoup(html, 'html.parser')
+        #找到所有的a标签列表（包含该套图的url信息）
+        A = soup.find_all('a',attrs={'class':'proimg'})
+        #提取所有<a>标签中的[‘href’]属性的参数
+        for a in A:
+            url = T_url + a.attrs['href']
+            T_list.append(url)
     except:
+        traceback.print_exc()
+        print('getALLlist()出错！')
+    
+#下载该页面所有套图方法!!!!!!!!!!!!!!!!!!!!!!!!!
+def downloadALL(T_List, root):
+    #存储一个套图所有图片的url
+    try:
+        #for i in range(len(T_List)):
+        for i in range(15):
+            List = []
+            getPictureUrl_List(List, T_List[i])
+            path = root + str(i+1)+'/'
+            print('正在下载第' + str(i+1) + '个套图')
+            downloadPicture(List, path)
+    except:
+        traceback.print_exc()
         print('')
 
 
 def main():
-    url = 'http://pic.yxdown.com/list/2_0_1.html'
+    url = 'http://pic.yxdown.com'
     #root为所有套图都在装在该文件夹
     root = '/home/dc/桌面/爬虫练习(一)/图片/'
     #该列表存储所有套图的url
-    T_List = []
-    T_List = getALLlist(url)
+    T_list = []
+    getALLlist(T_list, url)
     downloadALL(T_list, root)
 
 main()
